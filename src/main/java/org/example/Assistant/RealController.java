766e39861ffe7c907cf7f28cef9864395fddd173
ㅇi쀼z1 <yaewon0411@naver.com>
2024-01-16 14:28:44 +0900

    update

--- src/main/java/org/example/Assistant/RealController.java
+++ src/main/java/org/example/Assistant/RealController.java
@@ -155,7 +155,6 @@ public class RealController {
             cnt++;
         }
         //음성으로 질문한 거라면
-        System.out.println("getMessageDto.getIsVoice() = " + getMessageDto.getIsVoice());
         if(getMessageDto.getIsVoice().equals("true")){
             //byte[] speech = assistantService.createSpeech2(new AudioRequestDto(chatDto.getAnswer()));
             System.out.println("음성 인터페이스 전환");
@@ -205,8 +204,6 @@ public class RealController {
 
         Assistant findOne = realService.findById(assistantId);
         String setInstruction = "";
-        System.out.println("modifyRequestDto.toString() = " + modifyRequestDto.toString());
-        System.out.println("modifyRequestDto.getFile2() = " + modifyRequestDto.getFile2());
 
         //튜터 성향에 관한 수정 사항 검증
         Personality personality = findOne.getPersonality();
@@ -214,12 +211,10 @@ public class RealController {
 
         //instruction 변경 검증
         if(!modifyRequestDto.getInstruction().equals(findOne.getInstruction())){
-            System.out.println("instruction 변경");
             realService.modifyAssistantInstruction(modifyRequestDto.getInstruction(), assistantId);
 
             //personality, speechLevel 둘 다 수정된 경우
             if(personality != null && speechLevel != null && !personality.equals(modifyRequestDto.getPersonality()) && !speechLevel.equals(modifyRequestDto.getSpeechLevel())){
-                System.out.println("첫 번째 조건문 걸림");
                 setInstruction = assistantService.setInstruction(modifyRequestDto.getInstruction(), modifyRequestDto.getPersonality().toString(), modifyRequestDto.getSpeechLevel().toString());
                 modifyRequestDto.setInstruction(setInstruction);
                 realService.modifyAssistantPersonality(modifyRequestDto.getPersonality(), assistantId);
@@ -227,29 +222,24 @@ public class RealController {
             }
             //personality만 수정된 경우
             else if(personality != null && speechLevel != null && !personality.equals(modifyRequestDto.getPersonality()) && speechLevel.equals(modifyRequestDto.getSpeechLevel())){
-                System.out.println("두 번째 조건문 걸림");
                 setInstruction = assistantService.setInstruction(modifyRequestDto.getInstruction(), modifyRequestDto.getPersonality().toString(), speechLevel.toString());
                 modifyRequestDto.setInstruction(setInstruction);
                 realService.modifyAssistantPersonality(modifyRequestDto.getPersonality(), assistantId);
             }
             //speechLevel만 수정된 경우
             else if(personality != null && speechLevel != null && personality.equals(modifyRequestDto.getPersonality()) && !speechLevel.equals(modifyRequestDto.getSpeechLevel())){
-                System.out.println("세 번째 조건문 걸림");
                 setInstruction = assistantService.setInstruction(modifyRequestDto.getInstruction(), personality.toString(), modifyRequestDto.getSpeechLevel().toString());
                 modifyRequestDto.setInstruction(setInstruction);
                 realService.modifyAssistantSpeechLevel(modifyRequestDto.getSpeechLevel(), assistantId);
             }
             //둘 다 수정 안된 경우
             else {
-                System.out.println("둘 다 수정 안됨");
                 setInstruction = assistantService.setInstruction(modifyRequestDto.getInstruction(), findOne.getPersonality().toString(), findOne.getSpeechLevel().toString());
-                System.out.println("setInstruction = " + setInstruction);
                 modifyRequestDto.setInstruction(setInstruction);
             }
         } else{ //insturction이 변경 안 된 경우
             //personality, speechLevel 둘 다 수정된 경우
             if(personality != null && speechLevel != null && !personality.equals(modifyRequestDto.getPersonality()) && !speechLevel.equals(modifyRequestDto.getSpeechLevel())){
-                System.out.println("첫 번째 조건문 걸림");
                 setInstruction = assistantService.setInstruction(findOne.getInstruction(), modifyRequestDto.getPersonality().toString(), modifyRequestDto.getSpeechLevel().toString());
                 modifyRequestDto.setInstruction(setInstruction);
                 realService.modifyAssistantPersonality(modifyRequestDto.getPersonality(), assistantId);
@@ -257,14 +247,12 @@ public class RealController {
             }
             //personality만 수정된 경우
             else if(personality != null && speechLevel != null && !personality.equals(modifyRequestDto.getPersonality()) && speechLevel.equals(modifyRequestDto.getSpeechLevel())){
-                System.out.println("두 번째 조건문 걸림");
                 setInstruction = assistantService.setInstruction(findOne.getInstruction(), modifyRequestDto.getPersonality().toString(), speechLevel.toString());
                 modifyRequestDto.setInstruction(setInstruction);
                 realService.modifyAssistantPersonality(modifyRequestDto.getPersonality(), assistantId);
             }
             //speechLevel만 수정된 경우
             else if(personality != null && speechLevel != null && personality.equals(modifyRequestDto.getPersonality()) && !speechLevel.equals(modifyRequestDto.getSpeechLevel())){
-                System.out.println("세 번째 조건문 걸림");
                 setInstruction = assistantService.setInstruction(findOne.getInstruction(), personality.toString(), modifyRequestDto.getSpeechLevel().toString());
                 modifyRequestDto.setInstruction(setInstruction);
                 realService.modifyAssistantSpeechLevel(modifyRequestDto.getSpeechLevel(), assistantId);
@@ -315,7 +303,6 @@ public class RealController {
         ResponseEntity<Object> res = assistantService.modifyAssistant(assistantId, modifyRequestDto);
         return ResponseEntity.ok(res);
     }
-
 
     //사용자가 튜터 이미지를 변경했을 때만 작돟하도록 프론트에서 설정
     @PutMapping("/assistants/{assistantId}/info/page/image")
